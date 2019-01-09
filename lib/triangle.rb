c7793e8ec19c94812fd45a59d8d7cae43139a12e
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-09 23:40:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
   
   def kind()
-    if(@side1==@side2 && @side2==@side3 && @side1==@side3)
+    if(@side1 == @side2 && @side2 == @side3 && @side1 == @side3)
       :equilateral
     end
     if(((@side1 == @side2) && (@side3 != @side1)) || 
