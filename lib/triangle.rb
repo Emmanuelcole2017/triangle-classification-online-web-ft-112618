67340cae95e2431d229d4439b29bf75e092c903d
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-09 23:43:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,5 +12,9 @@ class Triangle
     if(@side1 == @side2 && @side2 == @side3 && @side1 == @side3)
       :equilateral
     end
+    if(@side2 == @side3 && @side1 != @side3)
+      :isosceles
+    end
+    
   end
 end
