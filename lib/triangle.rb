9a4f223807e34fc1929d13b9d41fa9093862a2e5
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-10 00:04:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,8 @@ class Triangle
     
     if(@side1 == 0 && @side2 == 0 && @side3 == 0)
       raise TriangleError
+    elsif(@side1 < 0 || @side2 < 0 || @side3 < 0)
+      raise TriangleError
     elsif(@side1 == @side2 && @side2 == @side3 && @side1 == @side3)
       :equilateral
     elsif((@side2 == @side3 && @side1 != @side3) ||
